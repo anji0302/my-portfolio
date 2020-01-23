8b62f746249ff9bd06398a3e61bf44c662fa2323
anji0302 <madhu.anji508@gmail.com>
2020-01-22 20:28:13 -0500

    initial setup

---
+++ src/components/Footer.js
@@ -0,0 +1,27 @@
+import React from 'react';
+// import logo from '../logo.svg';
+import '../assets/scss/footer.scss';
+
+function Footer() {
+  return (
+    <div className="footer">Footer comp</div>
+    // <div className="Footer">
+    //   <header className="Footer-header">
+    //     <img src={logo} className="Footer-logo" alt="logo" />
+    //     <p>
+    //       footer Edit <code>src/Footer.js</code> and save to reload.
+    //     </p>
+    //     <a
+    //       className="Footer-link"
+    //       href="https://reactjs.org"
+    //       target="_blank"
+    //       rel="noopener noreferrer"
+    //     >
+    //       Learn React
+    //     </a>
+    //   </header>
+    // </div>
+  );
+}
+
+export default Footer;
